781cbc9b4de94dc72a237b08d3997dc7163a1025
linzoldyck8-ship-it <linzoldyck8@gmail.com>
2026-09-17 16:14:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -26,10 +26,11 @@ RANGOS_OVERWATCH = [
     "Plata 5", "Plata 4", "Plata 3", "Plata 2", "Plata 1",
     "Oro 5", "Oro 4", "Oro 3", "Oro 2", "Oro 1",
     "Platino 5", "Platino 4", "Platino 3", "Platino 2", "Platino 1",
+    "Esmeralda 5","Esmeralda 4","Esmeralda 3","Esmeralda 2","Esmeralda 1",
     "Diamante 5", "Diamante 4", "Diamante 3", "Diamante 2", "Diamante 1",
     "Maestro 5", "Maestro 4", "Maestro 3", "Maestro 2", "Maestro 1",
     "Gran Maestro 5", "Gran Maestro 4", "Gran Maestro 3", "Gran Maestro 2", "Gran Maestro 1",
-    "Campeón"
+    "TOP 500"
 ]
 
 # DICCIONARIO PARA ETIQUETAR LOS IDS SEGÚN LA DIVISIÓN
