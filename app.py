e4b83e986c8f7b9fed25d3b46ba595c8a8d50948
linzoldyck8-ship-it <linzoldyck8@gmail.com>
2026-09-17 16:27:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def es_correo_valido(correo):
 HORARIOS_DIVISIONES = {
     "Valorant": {
         "División A": {"horario": "20:00 - 23:00 (Lun a Vie)", "rango": "Inmortal"},
-        "División B": {"horario": "18:00 - 21:00 (Lun a Vie)", "rango": "Ascendente"},
+        "División B": {"horario": "21:00 - 23:00 (Lun a Vie)", "rango": "plata"},
         "División C": {"horario": "16:00 - 19:00 (Sáb y Dom)", "rango": "Diamante"}
     },
     "CS GO": {
